192d6716b1332be125947afb7dda239788397725
Nima <nimaazimi.core@gmail.com>
2026-07-23 19:56:45 +0330

    chore: remove temporary require guest bypass

--- apps/web/src/app/routes/auth/route.tsx
+++ apps/web/src/app/routes/auth/route.tsx
@@ -1,9 +1,9 @@
-// import { requireGuest } from "#/features/auth";
+import { requireGuest } from "#/features/auth";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/auth")({
   beforeLoad: () => {
-    // requireGuest();
+    requireGuest();
   },
   component: Outlet,
 });
