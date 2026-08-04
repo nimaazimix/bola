c56f424578870a68880b28e1eb36d4e93166306c
Nima <nimaazimi.core@gmail.com>
2026-08-04 21:58:33 +0330

    feat(auth): add sign out button

--- apps/web/src/features/auth/index.ts
+++ apps/web/src/features/auth/index.ts
@@ -1,5 +1,6 @@
 export * from "./components/sign-in";
 export * from "./components/sign-up";
 export * from "./api/use-refresh";
+export * from "./api/use-sign-out";
 export * from "./hooks/use-email-verification";
 export * from "./lib/resolve-destination";
